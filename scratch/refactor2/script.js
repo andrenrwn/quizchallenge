55cb1a6d60c2f682094e7c8798929b5cdb1bf751
anrwn <andrenrwn@users.noreply.github.com>
2023-09-03 08:15:53 +0800

    Added timer countdown feature

--- scratch/refactor2/script.js
+++ scratch/refactor2/script.js
@@ -1,4 +1,6 @@
+// ----------------
 // global variables
+// ----------------
 var stackedCards = document.querySelector(".stacked-cards"); // cards of displayed questions
 var quizdata = document.querySelector("#quizdata").children; // all quiz questions from HTML
 var order = Array.from(Array(quizdata.length).keys()); // track question order
@@ -6,6 +8,11 @@ var currentcard = 0; // tracks question to ask
 
 var highscores = {};
 var yourscore = 0;
+
+var timeleft; // maximum time
+var timededuct = parseFloat(document.getElementById("timededuct").textContent);
+var timedispEl = document.getElementById("timerdisplay");
+var timerObj = null;
 
 // Fisher-Yates shuffle, adapted from https://javascript.info/task/shuffle
 // shuffle the order only for quiz cards
@@ -24,22 +31,24 @@ function shuffle(arr) {
 // Draw stacked cards
 //   pullup: false --- draw stacked cards
 //   pullup: true --- draw a pulled up card with the rest pushed down
+//   Idea from colin: https://codepen.io/colin/pen/bdxoZL
 function stackcards(pullup) {
 	var allstackedcards = document.querySelectorAll(".card--added");
 	var z = -allstackedcards.length;
-
 	var opacity = 1;
 	var yposition = 0;
 	var zposition = 0;
+
 	for (var i of allstackedcards) {
 		// stack cards
 		opacity = (1.0 + ((z + 1) / 10.0));
 		yposition = ((z + 1) * 10);
 		zposition = (z + 1) * 32;
 		if (!pullup) {
+			// render stacked cards
 			i.style = "transform: translate3d(0, " + yposition + "px," + zposition + "px) scale(1); opacity: " + opacity + ";";
 		} else {
-			// pull up clicked card from stack, push down others
+			// pull up clicked card from stack, push down other stacked cards
 			if (i.classList.contains("pulled-up")) {
 				i.style = "transform: translate3d(0, " + (-90 + yposition) + "px," + zposition + "px) scale(1); opacity: 1; min-height:550px;";
 			} else {
@@ -50,10 +59,24 @@ function stackcards(pullup) {
 	}
 }
 
+// lay out all quiz cards for review
+function layoutcards() {
+	var quizdisplay = document.getElementsByClassName("stacked-cards");
+	var allstackedcards = document.getElementsByClassName("card--added");
+
+	quizdisplay[0].style = "height: auto; transform: scale(0.5); transform-origin: 50% 0;";
+	quizdisplay[0].parentElement.style = "height:auto;";
+
+	for (var i of allstackedcards) {
+		//i.setAttribute('style', "background-color: green; position: relative; opacity: 1; transform: translate3d(0px,0px,0px);");
+		i.style = "position: relative; opacity: 1; margin-bottom: 5px;";
+	}
+}
+
 // display a new quiz card (currentcard)
 function quizcard() {
 	let newcard = document.createElement("div");
-	newcard.innerHTML = quizdata[currentcard].innerHTML;
+	newcard.innerHTML = quizdata[order[currentcard]].innerHTML;
 	newcard.classList.add("card");
 	let frontcard = stackedCards.appendChild(newcard);
 
@@ -73,64 +96,78 @@ function quizcard() {
 	return frontcard;
 }
 
+// render a pulled up card
 function pullupcard(event) {
-	console.log("pullupcard event");
-	//console.log(event.target);
 	//event.preventDefault();
 
 	// toggle pullup
 	event.currentTarget.classList.toggle("pulled-up");
 
 	if (event.currentTarget.classList.contains("pulled-up")) {
-		console.log("pull up stack cards");
 		stackcards(true);
 	} else {
-		console.log("return to stacked cards");
 		stackcards(false);
 	}
 }
 
+// Evaluate a user's answer when they click on <li> items and add score
+// Correct answers earn the data-answer value if specified, or 1 point if unspecified
 function evaluatequiz(event) {
-	console.log("Evaluate: " + event.target.outerHTML);
-	console.log("  answer: " + event.target.dataset.answer);
-
 	var evaluation = "";
 	if (!event.target.hasAttribute("data-answer")) {
 		evaluation = "Your choice: " + event.target.textContent + ", is incorrect";
-	} else if (event.target.dataset.answer==="") {
-        evaluation = "Your choice: " + event.target.textContent + ", is correct";
-        yourscore++;
-        document.querySelector("#yourscore").textContent = yourscore;
+		timeleft -= timededuct;
+	} else if (event.target.dataset.answer === "") {
+		evaluation = "Your choice: " + event.target.textContent + ", is correct";
+		yourscore++;
+		document.querySelector("#yourscore").textContent = yourscore.toFixed(2);
 	} else {
-        evaluation = "Your choice: " + event.target.textContent + ", is only partially correct";
-        yourscore += parseFloat(event.target.dataset.answer);
-        document.querySelector("#yourscore").textContent = yourscore;
+		evaluation = "Your choice: " + event.target.textContent + ", is only partially correct";
+		yourscore += parseFloat(event.target.dataset.answer);
+		document.querySelector("#yourscore").textContent = yourscore.toFixed(2);
 	}
 
-	console.log("          " + evaluation);
-
 	let resultdiv = document.createElement("div");
+	resultdiv.classList.add("result");
 	resultdiv.innerHTML = "<hr><i>" + evaluation + "</i>";
 	return resultdiv;
 }
 
+// countdown timer function is called periodically by timerObj's setinterval
+function countdown() {
+	timedispEl.innerHTML = timeleft.toFixed(2);
+	timeleft -= 0.10; // ten times a second
 
+	// have we reached the finish card?
+	if ((currentcard >= (order.length - 1)) || (timeleft <= 0)) {
+		if (timeleft <= 0) {
+			timeleft = 0;
+			timedispEl.innerHTML = timeleft.toFixed(2);
+			currentcard = order.length - 1;
+			quizcard();
+			stackcards(false);
+		}
+		clearInterval(timerObj);
+	}
+}
 
+// Main quiz user-interface handler
+//   Handles user clicks on displayed quiz cards
 function nextcard(event) {
-	console.log("click event on card");
 	event.preventDefault();
 	event.stopPropagation(); // stop the click from triggering other events?
 
 	// pullup card if clicked anywhere else than the <li> options
-	if (event.target.nodeName != "LI" || event.currentTarget.classList.contains("pulled-up")) {
-		pullupcard(event);
+	if (!(event.target.nodeName === 'LI' || event.target.nodeName === 'INPUT') ||
+		event.currentTarget.classList.contains("pulled-up")) {
+		if (event.currentTarget.nextSibling !== null) { // don't pullup front card
+			pullupcard(event);
+		}
 		return;
 	}
 
-	var thiscardtype = quizdata[currentcard].classList;
+	var thiscardtype = quizdata[order[currentcard]].classList;
 	var result; // result of quiz evaluation
-
-	console.log("type: " + quizdata[currentcard].classList.item(0));
 
 	// if this is a quiz card, evaluate it
 	if (thiscardtype.contains('quiz')) {
@@ -138,32 +175,40 @@ function nextcard(event) {
 		event.currentTarget.append(result);
 	}
 
-	// if this is a start card, reset the score
+	// if this is a start card, reset the score and start timer
 	if (thiscardtype.contains('start')) {
 		yourscore = 0;
-		document.querySelector("#yourscore").textContent = yourscore;
+		document.querySelector("#yourscore").textContent = yourscore.toFixed(2);
+
+		// start timer once user starts quiz
+		if (event.target.nodeName === 'LI') {
+			timerObj = setInterval(countdown, 100);
+		}
 	}
 
 	// finish the quiz and collect initials
 	if (thiscardtype.contains('finish')) {
 		var initials = document.querySelector("#initials");
-		console.log("initials: " + initials.value);
-		finishquiz();
+
+		if (event.target.nodeName == "LI") {
+			finishquiz();
+			return; // goto out - do not refresh with stacked cards
+		}
+	} else {
+		// advance quiz question index only if this isn't a finish card
+		currentcard++;
+
+		// console.log("card advancing to " + currentcard + "order index" + order[currentcard]);
+
+		quizcard();
 	}
 
-	// advance quiz question index
-	currentcard++;
-
-	console.log("card advancing to " + currentcard);
-
-	let frontcard = quizcard();
-	stackcards(false);
+	stackcards(false); // refresh page
 }
 
-function finishquiz() {
-	alert("quiz finished!");
-}
-
+// Debug tools - Popout card system
+//   Adapted from colin: https://codepen.io/colin/pen/bdxoZL
+/*
 function addCard(event) {
 	var addstring = "";
 	if (event.target.id == "js-add-card") {
@@ -204,46 +249,55 @@ document.getElementById("js-remove-card").addEventListener("click",
 // --------------------
 /* High score system */
 
+// clear high scores from localstorage and refresh highscore display
 function resetscores() {
 	highscores = {};
 	localStorage.setItem("highscores", "");
 	localStorage.setItem("hiscore", 0);
-	document.querySelector("#hiscore").textContent = "0";
+	document.querySelector("#hiscore").textContent = "0.00";
 	yourscore = 0;
-	document.querySelector("#yourscore").textContent = yourscore;
+	document.querySelector("#yourscore").textContent = yourscore.toFixed(2);
 	refreshhiscores();
 }
 
+// redraw highscore table
 function refreshhiscores() {
 	var scorerow = "<tr><th>Name</th><th>High Score</th></tr>";
-
 	gethighscores();
-
 	for (var i in highscores) {
 		scorerow += "<tr>";
 		scorerow += "<td>" + i + "</td>";
 		scorerow += "<td>" + highscores[i] + "</td></tr>";
 	}
-
 	document.getElementById("scoretable").innerHTML = scorerow;
 }
 
+// get highscore from localstorage
 function gethighscores() {
 	var hstr = localStorage.getItem("highscores");
+	var unsorted = {};
 	if (hstr != "") {
-		highscores = JSON.parse(hstr);
+		unsorted = JSON.parse(hstr);
 	};
+
+	// sort for ES10 from
+	// https://stackoverflow.com/questions/1069666/sorting-object-property-by-values
+	highscores = Object.fromEntries(
+		Object.entries(unsorted).sort(([, a], [, b]) => b - a));
 }
 
 function storehighscores() {
 	localStorage.setItem("highscores", JSON.stringify(highscores));
 }
 
+// ----------------------------------
 // Finish the quiz, store high scores
 function finishquiz() {
+
+	layoutcards();
+
 	var initials = document.querySelector("#initials");
-	console.log("initials: " + initials.value);
-	if (initials.value != null) {
+	if (initials.value !== null && initials.value !== "") {
 		if (!(initials.value in highscores)) {
 			highscores[initials.value] = yourscore;
 		} else {
@@ -258,10 +312,15 @@ function finishquiz() {
 		localStorage.setItem(document.querySelector("#initials").value, yourscore);
 	};
 	refreshhiscores();
+
+	if (confirm("Quiz finished. Ok to Restart. Cancel to Review.")) {
+		location.reload();
+	};
 }
 
-// ----------------------------------------
-// Accordion class from https://css-tricks.com/how-to-animate-the-details-element-using-waapi/
+// ----------------------
+// Accordion object class
+// Taken from https://css-tricks.com/how-to-animate-the-details-element-using-waapi/
 // https://codepen.io/Mamboleoo/pen/QWEpLqm
 
 class Accordion {
@@ -377,28 +436,31 @@ class Accordion {
 	}
 }
 
-// /Accordion class
-// add an accordion to all <details> sections to animate them
+// Add accordion to all <details> sections to animate them
 document.querySelectorAll('details').forEach((el) => {
 	new Accordion(el);
 });
 
-
+/* Debug tools - high score
 // populate local store high scores with dummy data ( for debugging )
 function populatehighscores() {
-    for (var i=0; i< 10; i++) {
-        highscores[String.fromCharCode(i+97)] = "0." + i.toString();
-    }
-    storehighscores();
+	for (var i = 0; i < 10; i++) {
+		highscores[String.fromCharCode(i + 97)] = "0." + i.toString();
+	}
+	storehighscores();
 }
 populatehighscores();
+*/
 
-
-// Begin executing the game
-
+// --------------------
+// Start quiz execution
 function init() {
+	// set up high scores from localstore
 	gethighscores();
 	refreshhiscores();
+
+	// get timer from HTML
+	timeleft = parseFloat(document.getElementById("timetotal").textContent);
 
 	// shuffle quiz card order
 	shuffle(order);
